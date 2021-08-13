67109b31bd61bff0e0643705b04170cd46d33881
haerong22 <haerong22@naver.com>
2021-08-13 15:37:30 +0900

    init

---
+++ src/main/java/com/example/artcoin/ArtcoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.artcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtcoinApplication.class, args);
+    }
+
+}
