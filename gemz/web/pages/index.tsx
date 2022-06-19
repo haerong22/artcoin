b3f87aaddd4f0fbb81e5912d1e8f8251350ec261
haerong22 <haerong22@naver.com>
2022-06-19 15:28:53 +0900

    #5 gem nft: web setting - connect kaikas

---
+++ gemz/web/pages/index.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useAccount } from "../hooks";
+
+const Home: NextPage = () => {
+  const { account } = useAccount();
+
+  useEffect(() => {
+    console.log(account);
+  });
+
+  return <Box>Home</Box>;
+};
+
+export default Home;
