9a10f0ed9a3c81dc8c605935d08363e7cb79e309
haerong22 <haerong22@naver.com>
2022-06-19 16:35:46 +0900

    #5 gem nft: web setting - connect contract(caver-js)

--- gemz/web/pages/index.tsx
+++ gemz/web/pages/index.tsx
@@ -1,13 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useEffect } from "react";
-import { useAccount } from "../hooks";
+import { useAccount, useCaver } from "../hooks";
 
 const Home: NextPage = () => {
   const { account } = useAccount();
+  const { caver, mintGemTokenContract, saleGemTokenContract } = useCaver();
 
   useEffect(() => {
     console.log(account);
+    console.log(caver);
+    console.log(mintGemTokenContract);
+    console.log(saleGemTokenContract);
   });
 
   return <Box>Home</Box>;
