81e4f497c76916ac2c5d908214b9aba8b2021e63
haerong22 <haerong22@naver.com>
2023-06-08 19:22:34 +0900

    #8 lottery: lottery v2 api - sign transaction

--- lottery/lottery-v2-api-server/services/contract/ContractUtil.js
+++ lottery/lottery-v2-api-server/services/contract/ContractUtil.js
@@ -6,6 +6,10 @@ require("dotenv").config({
 const Web3 = require("web3");
 const path = require("path");
 const fs = require("fs");
+const { FeeMarketEIP1559Transaction } = require("@ethereumjs/tx");
+const Common = require("@ethereumjs/common").default;
+const { Chain, Hardfork } = require("@ethereumjs/common");
+const { chain } = require("lodash");
 
 class ContractUtil {
   constructor() {
@@ -42,6 +46,51 @@ class ContractUtil {
       const json = fs.readFileSync(`${dir}/${contractName}.json`);
       const instance = JSON.parse(json);
       return instance.abi;
+    } catch (err) {
+      console.error(`[${funcName}] err:`, err);
+    }
+  }
+
+  async signTransaction(signer, pk, to, gas, value, data) {
+    const funcName = "signTransaction";
+
+    try {
+      const nonce = await this.web3.eth.getTransactionCount(signer);
+      const priorityFee = this.web3.utils.toWei("1", "Gwei");
+      const pendingBlock = await this.web3.eth.getBlock("pending");
+      const baseFeePerGas = pendingBlock.baseFeePerGas;
+      const chainId = await this.web3.eth.net.getId();
+
+      const rawTx = {
+        nonce: this.web3.utils.toHex(nonce),
+        to: to,
+        maxPriorityFeePerGas: this.web3.utils.toHex(priorityFee),
+        maxFeePerGas: this.web3.utils.toHex(
+          Math.floor(baseFeePerGas * 1.01) + Number(priorityFee)
+        ),
+        gas: this.web3.utils.toHex(gas),
+        gasLimit: this.web3.utils.toHex(Math.floor(gas * 1.01)),
+        value: this.web3.utils.toHex(value),
+        data: data,
+        chainId: this.web3.utils.toHex(chainId),
+      };
+
+      console.log(`[${funcName}] rawTx: ${JSON.stringify(rawTx)}`);
+
+      const common = new Common({
+        chain: Chain.Goerli,
+        hardfork: Hardfork.London,
+      });
+      const tx = FeeMarketEIP1559Transaction.fromTxData(rawTx, { common });
+      const signedTx = tx.sign(Buffer.from(pk, "hex"));
+      const serializeTx = "0x" + signedTx.serialize().toString("hex");
+      console.log(
+        `[${funcName}] signedTx: ${JSON.stringify(
+          signedTx
+        )}, serializedTx: ${serializeTx}`
+      );
+
+      return serializeTx;
     } catch (err) {
       console.error(`[${funcName}] err:`, err);
     }
