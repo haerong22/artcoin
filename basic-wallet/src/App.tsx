ff81340a407ee601f74dead8b390da577a3d36c5
haerong22 <haerong22@naver.com>
2023-06-15 19:56:22 +0900

    #10 wallet: index page

--- basic-wallet/src/App.tsx
+++ basic-wallet/src/App.tsx
@@ -1,5 +1,14 @@
+import { Card, CardContent } from "@mui/material";
+import "./App.scss";
+
 function App() {
-  return <div>Hello, world!!!</div>;
+  return (
+    <div className="App">
+      <Card className="AppBody">
+        <CardContent>Hello world</CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
--- basic-wallet/src/index.tsx
+++ basic-wallet/src/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MetaMaskProvider } from "metamask-react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const darktheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#2b32b2",
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darktheme}>
+      <MetaMaskProvider>
+        <App />
+      </MetaMaskProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
