f51f59425c8512a654e4b5afdab77e4a50a7d749
haerong22 <haerong22@naver.com>
2023-06-27 19:36:43 +0900

    #14 defi: csmm - add liquidity

---
+++ defi/basic/test/Exchange.ts
@@ -0,0 +1,38 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+import { Exchange } from "../typechain-types/contracts/Exchange";
+import { Token } from "../typechain-types/contracts/Token";
+
+const toWei = (value: number) => ethers.utils.parseEther(value.toString());
+
+const getBalance = ethers.provider.getBalance;
+
+describe("Exchange", () => {
+  let owner: any;
+  let user: any;
+  let exchange: Exchange;
+  let token: Token;
+
+  beforeEach(async () => {
+    [owner, user] = await ethers.getSigners();
+
+    const TokenFactory = await ethers.getContractFactory("Token");
+    token = await TokenFactory.deploy("BobbyToken", "BOBBY", toWei(1000000));
+    await token.deployed();
+
+    const ExchangeFactory = await ethers.getContractFactory("Exchange");
+    exchange = await ExchangeFactory.deploy(token.address);
+    await exchange.deployed();
+  });
+
+  describe("addLiquidity", async () => {
+    it("add Liquidity", async () => {
+      await token.approve(exchange.address, toWei(1000));
+      await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
+
+      expect(await getBalance(exchange.address)).to.equal(toWei(1000));
+      expect(await token.balanceOf(exchange.address)).to.equal(toWei(1000));
+    });
+  });
+});
